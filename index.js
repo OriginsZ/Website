af863b907b2f000ff5d45ea54388ad24ba81d816
OffBranded <141286865+OffBranded@users.noreply.github.com>
2024-09-09 18:20:45 -0600

    Add files via upload
    faq section :D

--- index.js
+++ index.js
@@ -13,3 +13,28 @@ function showSection(sectionId) {
 
 // Show the home section by default
 showSection('home');
+
+
+
+
+function showSection(sectionId) {
+    const sections = document.querySelectorAll('.section');
+    sections.forEach(section => {
+        section.classList.remove('visible');
+    });
+    const selectedSection = document.getElementById(sectionId);
+    selectedSection.classList.add('visible');
+}
+
+function toggleFAQ(sectionId) {
+    const section = document.getElementById(sectionId);
+    if (section.classList.contains('visible')) {
+        section.classList.remove('visible');
+    } else {
+        document.querySelectorAll('.faq-body').forEach(body => body.classList.remove('visible'));
+        section.classList.add('visible');
+    }
+}
+
+// Show the home section by default
+showSection('home');
